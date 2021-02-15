280c5bdc562559e8718a083a992e9026e2edfacc
PopperLab <lucy.insl@gmail.com>
2021-02-15 19:43:53 -0300

    Map y promises

--- src/mocks/productList.js
+++ src/mocks/productList.js
@@ -1,9 +1,9 @@
 const productList = [
-  { id: 1, name: "Red Bull", description: "lorem", price: 5000, url: ""},
-  { id: 2, name: "Monster", description:"lorem", price: 2000, url: "" },
-  { id: 3, name: "Rad60", description: "lorem", price: 2500, url: "" },
-  { id: 4, name: "Speed", description: "lorem", price: 3000, url: "" },
-  { id: 5, name: "Blink", description: "lorem", price: 4000, url: "" },
+  { id: 1, name: "Red Bull", description: "lorem", price: 130, url: ""},
+  { id: 2, name: "Monster", description:"lorem", price: 120, url: "" },
+  { id: 3, name: "Rad60", description: "lorem", price: 100, url: "" },
+  { id: 4, name: "Speed", description: "lorem", price: 90, url: "" },
+  { id: 5, name: "Blink", description: "lorem", price: 90, url: "" },
 ];
 
 export default productList;
